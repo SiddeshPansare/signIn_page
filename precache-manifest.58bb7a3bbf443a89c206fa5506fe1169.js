547175b393e1e65c4c251d047acb62123c6acc28
SiddeshPansare <40264381+SiddeshPansare@users.noreply.github.com>
2018-12-09 12:54:59 +0530

    Add files via upload

---
+++ precache-manifest.58bb7a3bbf443a89c206fa5506fe1169.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "eb9023ab5ffce3e28ef8",
+    "url": "/static/js/main.eb9023ab.chunk.js"
+  },
+  {
+    "revision": "9bd75c8fa8c887d483ad",
+    "url": "/static/js/1.9bd75c8f.chunk.js"
+  },
+  {
+    "revision": "eb9023ab5ffce3e28ef8",
+    "url": "/static/css/main.195e9658.chunk.css"
+  },
+  {
+    "revision": "021b60e91b484139d3d7cb7f75c20c65",
+    "url": "/index.html"
+  }
+];
